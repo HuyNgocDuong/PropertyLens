be0f7d8b593e2931a39075c668a5671a3de3c74c
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2024-10-31 18:49:36 +1100

    Change Logo

--- FrontEnd/PropertyLens-Website/src/components/Header.jsx
+++ FrontEnd/PropertyLens-Website/src/components/Header.jsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import { AppBar, Toolbar, Container, Button, Box } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
-import { styled } from '@mui/system';
+import React from "react";
+import { AppBar, Toolbar, Container, Button, Box } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { styled } from "@mui/system";
 
 // Import logo
-import Logo from '../assets/img/logo.svg';
+import Logo from "../assets/img/PropertyLens-Logo.png";
 
-const StyledAppBar = styled(AppBar)( {
-  backgroundColor: 'white',
-  boxShadow: 'none',
-  borderBottom: '1px solid #e5e7eb',
+const StyledAppBar = styled(AppBar)({
+  backgroundColor: "white",
+  boxShadow: "none",
+  borderBottom: "1px solid #e5e7eb",
 });
 
-const LogoLink = styled(RouterLink)( {
-  display: 'flex',
-  alignItems: 'center',
+const LogoLink = styled(RouterLink)({
+  display: "flex",
+  alignItems: "center",
 });
 
-const StyledButton = styled(Button)( ({ theme }) => ( {
-  textTransform: 'none',
-  '&.login': {
-    color: '#7c3aed', // Violet-700
-    '&:hover': {
-      color: '#6d28d9', // Violet-800 on hover
+const StyledButton = styled(Button)(({ theme }) => ({
+  textTransform: "none",
+  "&.login": {
+    color: "#7c3aed", // Violet-700
+    "&:hover": {
+      color: "#6d28d9", // Violet-800 on hover
     },
   },
-  '&.signup': {
-    backgroundColor: '#7c3aed', // Violet-700
-    color: 'white',
-    padding: '0.75rem 1rem',
-    borderRadius: '0.5rem',
-    '&:hover': {
-      backgroundColor: '#6d28d9', // Violet-800
+  "&.signup": {
+    backgroundColor: "#7c3aed", // Violet-700
+    color: "white",
+    padding: "0.75rem 1rem",
+    borderRadius: "0.5rem",
+    "&:hover": {
+      backgroundColor: "#6d28d9", // Violet-800
     },
   },
 }));
@@ -40,11 +40,18 @@ const Header = () => {
   return (
     <StyledAppBar position="static">
       <Container maxWidth="lg">
-        <Toolbar disableGutters sx={{ py: 2, display: 'flex', justifyContent: 'space-between' }}>
+        <Toolbar
+          disableGutters
+          sx={{ py: 2, display: "flex", justifyContent: "space-between" }}
+        >
           <LogoLink to="/">
-            <img src={Logo} alt="Logo" />
+            <img
+              src={Logo}
+              alt="Logo"
+              style={{ width: "auto", height: "60px" }}
+            />
           </LogoLink>
-          <Box sx={{ display: 'flex', gap: 2 }}>
+          <Box sx={{ display: "flex", gap: 2 }}>
             <StyledButton className="signup" component={RouterLink} to="/about">
               About Us
             </StyledButton>
